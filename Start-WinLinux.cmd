dda9cb63d7f791d713d55862b41f48e7a5efa8a8
Juan Luis Guerrero Minero <jlguerrero@gmail.com>
2021-10-26 10:03:46 +0200

    Update Start-WinLinux.cmd

--- Start-WinLinux.cmd
+++ Start-WinLinux.cmd
@@ -1,3 +1,4 @@
 @echo offs
+:: config.xlaunch file can be created running xlaunch.exe manually, and then save it.
 start /min C:\vcxsrv-64.1.20.9.0\xlaunch.exe -run C:\vcxsrv-64.1.20.9.0\config.xlaunch
 :: wsl
